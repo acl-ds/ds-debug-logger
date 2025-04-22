6bff808b3b53bbde27217ba7976dd9cac7f0824e
thanseer5529 <thanseerthanu5529@gmail.com>
2025-04-22 09:52:08 +0530

    payload getting from application side.

--- src/logger.js
+++ src/logger.js
@@ -1,27 +1,17 @@
 var { status_code } = require('./status_code.json')
-
-function log( userSession, params, es_client, index="ds_internal"){
-  const { status , message, type, mod="general" } = params
-
-  const insert_obj = {
-    '@timestamp': new Date().toISOString(),
-    status_code: status_code[status],
-    message: message,
-    type: type || 'info',
-    module: mod,
-    userId: userSession.id || 1,
-    tenant: userSession.customerId || '00000000-0000-0000-0000-000000000000',
-  }
-
+ 
+async function log( payload, es_client, index="ds_internal"){
+  
+ 
   try {
-    es_client.index({
+   await es_client.index({
       index,
       type: "_doc",
-      body: insert_obj
+      body: payload
     })
   } catch (error) {
     console.log(error)
   }
 }
-
-module.exports = { log }
+ 
+module.exports = { log } 
